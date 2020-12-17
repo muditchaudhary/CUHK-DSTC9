bd48f3c77bbbb8bbb11dfe2e80eadb879a473351
KristenZHANG <kristenzhang1997@gmail.com>
2020-12-17 22:34:17 +0800

    Re-organize Task2 code

--- Task_2/Module1/BERT-DE/baseline.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-if __name__ == "__main__":
-    if '--generate' in sys.argv:
-        from baseline import generate as main
-    else:
-        from baseline import main
-
-    main.main()

a64846928881a271c5f894e01314a736ace2f921
KristenZHANG <1155077075@link.cuhk.edu.hk>
2020-12-17 23:32:08 +0800

    Task2 BERT-D modification

--- Task_2/Module2/BERT-D/train_baseline.sh
+++ Task_2/Module2/BERT-D/train_baseline.sh
@@ -4,14 +4,11 @@
 
 # set path to dataset here
 version="baseline"
-dataroot="data"
+dataroot="combined8domainsall"
 num_gpus=1
 
-
-python3 -m torch.distributed.launch --nproc_per_node ${num_gpus} --master_addr 127.0.0.2 --master_port 29517 baseline.py \
+python3 -m torch.distributed.launch --nproc_per_node ${num_gpus} --master_addr 127.0.0.2 --master_port 29511 baseline.py \
     --negative_sample_method "all" \
     --params_file baseline/configs/selection/params.json \
-    --dataroot data \
-    --exp_name ks-all-${version}-dstc9all-256dh-128kn-4cand-10epoch
-
-
+    --dataroot combined8domainsall \
+    --exp_name ks-all-${version}-combined8domainsall-256dh-128kn-4cand-10epoch

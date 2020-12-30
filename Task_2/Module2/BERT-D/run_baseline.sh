4cb3c498cef9f529bd1975accd4b8a6baa61799a
KristenZHANG <1155077075@link.cuhk.edu.hk>
2020-12-30 09:56:13 +0800

    remove irrelevant data files

--- Task_2/Module2/BERT-D/run_baseline.sh
+++ Task_2/Module2/BERT-D/run_baseline.sh
@@ -9,7 +9,9 @@ python3 baseline.py --eval_only --checkpoint runs/ks-all-${version}-combined8dom
    --eval_all_snippets \
    --dataroot data_eval/ \
    --eval_dataset test \
-   --labels_file data_eval/test/team1-3step-Sunday.json \
-   --output_file pred/test-data/combined8domainsall-256dh-128kn-4cand-10epoch/team1-3step-team2-bert-Sunday.json
+   --labels_file data_eval/test/pseudo_input.ktd.json \
+# team1-3step-Sunday.json \
+   --output_file pred/test-data/combined8domainsall-256dh-128kn-4cand-10epoch/team2-bertd.json
+# team1-3step-team2-bert-Sunday.json
 
 
